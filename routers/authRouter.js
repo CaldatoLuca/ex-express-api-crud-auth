bbf1a3e66c16f3930de726c6e913e2696d16a575
CaldatoLuca <lucacaldato15@gmail.com>
2024-06-10 15:14:53 +0200

    feat: validazione in registrazione

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const validator = require("../middlewares/validator");
+const authValidation = require("../validations/authValidation");
 
-router.post("/register", authController.register);
+router.post(
+  "/register",
+  validator(authValidation.register),
+  authController.register
+);
 // router.get("/login", authController.index);
 
 module.exports = router;
---
+++ validations/authValidation.js
@@ -0,0 +1,73 @@
+const ValidationError = require("../exceptions/validationError");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const register = {
+  email: {
+    in: ["body"],
+    notEmpty: {
+      errorMessage: "Email must not be empty",
+      bail: true,
+    },
+    isEmail: {
+      errorMessage: "Email must be a valid email address",
+      bail: true,
+    },
+    custom: {
+      options: async (value) => {
+        const user = await prisma.user.findUnique({
+          where: { email: value },
+        });
+
+        if (user) {
+          throw new ValidationError(`Email already in use`, 400);
+        }
+
+        return true;
+      },
+    },
+  },
+  password: {
+    in: ["body"],
+    notEmpty: {
+      errorMessage: "Password must not be empty",
+      bail: true,
+    },
+    isString: {
+      errorMessage: "Password must be a string",
+      bail: true,
+    },
+    isLength: {
+      errorMessage: "Password must be at least 8 characters long",
+      options: { min: 8 },
+      bail: true,
+    },
+    matches: {
+      errorMessage:
+        "Password must contain at least one uppercase letter, one lowercase letter and one number",
+      options: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[a-zA-Z\d\W_]{8,}$/,
+      bail: true,
+    },
+  },
+  name: {
+    in: ["body"],
+    isString: {
+      errorMessage: "Name must be a string",
+      bail: true,
+    },
+    isLength: {
+      errorMessage: "Name must be at least 3 characters long",
+      options: { min: 3 },
+      bail: true,
+    },
+    matches: {
+      errorMessage: "Name must contain only letters and spaces",
+      options: /^[a-zA-Z\s]+$/,
+      bail: true,
+    },
+  },
+};
+
+module.exports = {
+  register,
+};
